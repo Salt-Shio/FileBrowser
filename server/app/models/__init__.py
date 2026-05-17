7913467ccf622a0b833826c30d92163cd6eba1cd
Salt-Shio <jawu959@gmail.com>
2026-05-18 06:16:50 +0800

    refactor: [models] 統一導出 UploadSession，建立包層級模型中轉匯出機制

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
 from .vfs import Folder, File
+from .upload import UploadSession
 
 # 統一在這裡導出，方便 database.py 的 init_db 自動建立資料表
-__all__ = ["User", "Folder", "File"]
+__all__ = ["User", "Folder", "File", "UploadSession"]
